dbcb5dfcec49053c85370ec804e465935f6101eb
Christopher Bergstrom <christopherrbergstrom@gmail.com>
2017-03-30 19:10:47 -0700

    added bio readme

--- js/readme.js
+++ js/readme.js
@@ -84,4 +84,18 @@ $(document).ready(function()
       });
     });
   });
+  $("#bioReadme").click(function()
+  {
+    $("body").append("<div id='readme'></div>").hide().fadeIn("slow");
+    $("#readme").append("<div id='readmeExit' class='fa fa-times'></div>");
+    $("#readme").append("<div id='readmeTitle'>About me</div>");
+    $("#readme").append("<div id='readmeText'>Hi, I'm Chris and I love coding. I discovered web development in 2015 and it's been a part of my life ever since.<br><br>Since then I have been on a wild journey that included quitting my job, moving from California to Colorado to attend a 6 month long full stack web development bootcamp called <a href='http://skilldistillery.com/' class='link' target='_blank'>Skill Distillery</a>, finishing the program but not being able to find a job, moving back to California to go back to my old job as a mail carrier, and now finally rediscovering my love for coding.<br><br>I believe that you are only limited by your imagination and that anything is possible if you believe in yourself and are willing to put in the work.<br><br>I am more determined now than I ever have been before to have a career in web development and fulfill my dreams of being a professional web developer.<br><br>In my free time I enjoy updating my website, making HTML games, riding my motorcycle, and going to the gym.</div>");
+    $("#readmeExit").click(function()
+    {
+      $("#readme").fadeOut(300, function()
+      {
+        $(this).remove();
+      });
+    });
+  });
 });
